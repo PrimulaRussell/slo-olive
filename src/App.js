b920cd0af912a85c15de5d70265515730fda40dc
Primula <59395386+PrimulaRussell@users.noreply.github.com>
2021-03-08 21:48:17 -0700

    added styled-components, began navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Header from "./components/header.js"
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,31 @@
-import Search from "../images/icons8-search-50.png";
-import Shopping from "../images/icons8-shopping-bag-75.png";
-import User from "../images/icons8-user-50.png";
+import React from 'react';
+import Search from "../images/icons8-search-25.png";
+import Shopping from "../images/icons8-shopping-bag-25.png";
+import User from "../images/icons8-user-25.png";
+import styled from 'styled-components';
+
+//Styled components//
 
 const Header = () => {
+
     return (
         <div>
+            <div>
             <h1>SLO OLIVE</h1>
             <img alt="Search" src={Search} />
             <img alt="Shopping Bag" src={Shopping}/>
             <img alt="User" src={User}/>
+            <a href="#">Sign up</a>
+            <a href="#">Log in</a>
+            </div>
+            <div>
+                <ul>
+                    <li>About</li>
+                    <li>Shop</li>
+                    <li>Visit Us</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </div>
     )
 }
