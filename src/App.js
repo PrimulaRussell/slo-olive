317f8397e04e3b28c3024b846ff51457f129cd60
Primula <primularussell@gmail.com>
2021-04-28 21:50:29 -0700

    added hero page background and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
+
+/*Components*/
+
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Hero></Hero>
+      <h1>hi</h1>
     </div>
   );
 };
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "../Styles/hero/hero.css";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div className="hero">
+      <p className="h1">San Luis Obispo Olive</p>
+    </div>
+  );
 };
 
 export default Hero;
